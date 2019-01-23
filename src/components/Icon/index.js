d3e4b2869c69ab99952a5fba8c5a30008f6809af
Marco Martínez <marcoamtz1@gmail.com>
2019-01-23 13:23:02 -0600

    Improved lazy loading.

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,60 +1,50 @@
 import React, { lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 
-const BrokenClouds = lazy(() => import('./BrokenClouds'));
-const ClearSky = lazy(() => import('./ClearSky'));
-const FewClouds = lazy(() => import('./FewClouds'));
-const Mist = lazy(() => import('./Mist'));
-const Rain = lazy(() => import('./Rain'));
-const ScatteredClouds = lazy(() => import('./ScatteredClouds'));
-const ShowerRain = lazy(() => import('./ShowerRain'));
-const Snow = lazy(() => import('./Snow'));
-const ThunderStorm = lazy(() => import('./ThunderStorm'));
-
 const Icon = ({ code, ...props }) => {
   let Component;
   switch (code) {
     case '04d':
     case '04n':
-      Component = BrokenClouds;
+      Component = lazy(() => import('./BrokenClouds'));
       break;
     case '01d':
     case '01n':
-      Component = ClearSky;
+      Component = lazy(() => import('./ClearSky'));
       break;
     case '02d':
     case '02n':
-      Component = FewClouds;
+      Component = lazy(() => import('./FewClouds'));
       break;
     case '50d':
     case '50n':
-      Component = Mist;
+      Component = lazy(() => import('./Mist'));
       break;
     case '10d':
     case '10n':
-      Component = Rain;
+      Component = lazy(() => import('./Rain'));
       break;
     case '03d':
     case '03n':
-      Component = ScatteredClouds;
+      Component = lazy(() => import('./ScatteredClouds'));
       break;
     case '09d':
     case '09n':
-      Component = ShowerRain;
+      Component = lazy(() => import('./ShowerRain'));
       break;
     case '13d':
     case '13n':
-      Component = Snow;
+      Component = lazy(() => import('./Snow'));
       break;
     case '11d':
     case '11n':
-      Component = ThunderStorm;
+      Component = lazy(() => import('./ThunderStorm'));
       break;
     default:
       break;
   }
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Icon placeholder</div>}>
       <Component {...props} />
     </Suspense>
   );
